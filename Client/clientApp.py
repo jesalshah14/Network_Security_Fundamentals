49059c4268deaeb5f856430ba98a569805d489ea
ademiltonnunes <ademilton.marcelo1@gmail.com>
2023-04-11 00:21:58 -0700

    Implement Digital Signature

--- Client/clientApp.py
+++ Client/clientApp.py
@@ -1,35 +1,77 @@
 from client import Client
 from RC4 import RC4Cipher
+from MD import MDAlgorithm
+import random
+
+class ClientApp:
+    def show_menu(self):
+        print("MENU")
+        print("1. CONTACT SERVER")
+        print("2. QUIT -  Exit system")        
+        print()
 
 def main() -> None:
+    app:ClientApp = ClientApp()
     rc4: RC4Cipher = RC4Cipher()
+    md: MDAlgorithm = MDAlgorithm()
     key :str =""
-    key = input("Insert the key for comunication:")
+    key = input("Insert the key for comunication: ")
 
     if key !="":
         client :Client = Client()
         while True:
             try:
-                # send a message to the server
-                message = input("Enter your message: ")
-                cipher = rc4.encryption(message,key)
-                client.socket.send(cipher.encode())
+                app.show_menu()
+                command_number: int = int(input("Please, enter a command number: "))
+                if command_number == 1:
 
-                # if the message is empty, close the connection
-                if not message:
+                    #generate a random number                   
+                    random_num = random.randint(1, 10000)
+                    message = str(random_num)
+                    print(f"Random number: {message}")
+
+                    #generate hash
+                    hash = md.create_hash(message)
+
+                    #digital signature
+                    message_signed = message + ","+hash
+                    print("Message Signed!")
+
+                    #encryption
+                    cipher = rc4.encryption(message_signed,key)
+                    print(f"Encrypted: {cipher}")
+
+                    # send a message to the server
+                    client.socket.send(cipher.encode())
+                    print("Message sent!")
+                    print()
+
+                    # receive data from the server
+                    response_cipher = client.socket.recv(1024).decode()
+                    response_decrypted = rc4.decryption(response_cipher,key)
+
+                    #Verify integrity
+                    split_response = response_decrypted.split(",")
+                    response = split_response[0]
+                    hash_response = split_response[1]
+                    integrity:bool = md.verify_integrity(response, hash_response)
+
+                    if integrity:
+                        print("Response.")
+                        print(f"Server: {response}") 
+                        print()  
+                    else:
+                        print("Message has been tampered with!")
+                        client.socket.close()
+                        break
+
+                elif command_number == 2:
                     client.socket.close()
-                    break
-
-                # receive data from the server
-                response_cipher = client.socket.recv(1024).decode()
-                response = rc4.decryption(response_cipher,key)
-                print(f"Server: {response}")
+                    break       
             except ValueError:
                 print("Something went wrong in the connection.")
     else:
-        print("The key you inform has a problem")
-
-    
+        print("The key you informed has a problem")
 
 if __name__ == "__main__":
     main()
